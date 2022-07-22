b3030b948f2a7cb538fea31d7cc5c368abfd105f
Anastasiia Nikita <anastasiya.folk@gmail.com>
2022-07-22 11:02:19 +0300

    Deploy

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,13 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: sans-serif;
+  }
+
   body {
     // background-color: ${({ theme }) => theme.colors.bg};
     // background: linear-gradient(to right, rgba(255,0,0,0), rgba(93, 173, 226, 1));
     display: grid;
     place-content: center;
     // text-align: center;
-    font-family: sans-serif;
+    margin: 0;
   }
 `
 
